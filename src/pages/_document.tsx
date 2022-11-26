68600e41e7360da2c16e62609ca9292d8f410108
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-11-26 18:51:55 +0900

    delete _document header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,14 +5,9 @@ export default function Document() {
     <Html lang="ja">
       <Head prefix="og: http://ogp.me/ns#">
         <meta charSet="utf-8" />
-        <link rel="manifest" href="/manifest.webmanifest" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/icon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icon-16x16.png" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#fafaf9" />
         <meta name="theme-color" content="#fafaf9" />
       </Head>
-      <body>
+      <body className="bg-[#fafaf9]">
         <Main />
         <NextScript />
       </body>
