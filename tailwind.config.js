942c8aa73bb90163cb1820ba1d6a7c7cc996a1e8
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-05-15 23:53:03 +0900

    chore: add tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "Helvetica Neue,Arial,Hiragino Kaku Gothic ProN,Hiragino Sans,Meiryo,sans-serif",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/container-queries")],
 }
