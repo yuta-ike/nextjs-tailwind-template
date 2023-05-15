f139dfa3b16f2c84ae5eb7975bcb8ca2984f0300
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-05-15 23:03:43 +0900

    chore: update lintstagedrc script

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,14 @@
 const path = require("path")
 
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`
+
+const buildPrettierCommand = (filenames) =>
+  `prettier --write --ignore-path .prettierignore ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" ")}`
+
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [
-    (filenames) =>
-      `next lint --fix --file ${filenames
-        .map((f) => path.relative(process.cwd(), f))
-        .join(" --file ")}`,
-    "yarn format:fix",
-  ],
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand, buildPrettierCommand],
+  "*.{js,jsx,ts,tsx,json}": [buildPrettierCommand],
 }
